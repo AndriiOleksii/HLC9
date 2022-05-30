2da0e2e78068eab72bd4f14504b4d53b068f0a82
AndriiOleksii <106560705+AndriiOleksii@users.noreply.github.com>
2022-05-31 00:53:16 +0300

    Add files via upload

---
+++ Helper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Concurrent;
+
+var file = File.ReadAllLines("Words.csv");
+
+file = file.Distinct().ToArray();
+
+var bag = new ConcurrentBag<string>();
+
+Parallel.For(0, file.Length, i =>
+{
+    var current = "{ \"create\" : { \"_index\" : \"words\", \"_id\" : \"" + i+1 + "\" } }\n";
+    current += "{ \"id\": \"" + i+1 + "\", \"word\" : \"" + file[i].Trim() + "\" }";
+
+    bag.Add(current);
+});
+
+var result = string.Join("\n", bag.ToList());
+
+await File.WriteAllTextAsync("data.txt", result);
